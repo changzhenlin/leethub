8c5a727455f971d8c3c5e4e869ffe4ac0924634b
changzhenlin <chang_zhenlin@126.com>
2022-12-15 21:38:04 +0800

    [20] 有效的括号

---
+++ 20.有效的括号.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=typescript
+ *
+ * [20] 有效的括号
+ */
+
+
+
+// @lc code=start
+function isValid(s: string): boolean {
+  const map:{[key: string]: string}  = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+  let arr = s.split('');
+  if (arr.length % 2 !== 0){
+    return false;
+  }
+  let stack:string[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    const char = arr[i];
+    if (map.hasOwnProperty(char)) {
+     if (stack.pop() !== map[char])
+      return false;
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
+};
+// @lc code=end
+
