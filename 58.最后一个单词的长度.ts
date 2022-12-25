8f5b49ef03042fa14b7e1ad94b5493755d2ae4b8
changzhenlin <chang_zhenlin@126.com>
2022-12-25 21:56:08 +0800

    搜索插入位置

--- 58.最后一个单词的长度.ts
+++ 58.最后一个单词的长度.ts
@@ -8,11 +8,14 @@
 function lengthOfLastWord(s: string): number {
   let arr = s.split('');
   let count = 0;
+  // 从后往前遍历
   for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] === ' ') {
+      // 如果是空格，且count为0，继续循环
       if (count === 0) continue;
       else break;
     }
+    // 如果不是空格，count++
     count++;
   }
   return count;
