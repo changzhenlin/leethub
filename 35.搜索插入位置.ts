57aabad98434dd340ec015a11376187f2122e3ad
changzhenlin <chang_zhenlin@126.com>
2022-12-26 11:06:29 +0800

    二分法查找注释

--- 35.搜索插入位置.ts
+++ 35.搜索插入位置.ts
@@ -8,7 +8,9 @@
 function searchInsert(nums: number[], target: number): number {
   let left = 0;
   let right = nums.length - 1;
+  // 二分查找
   while (left <= right) {
+    // math.floor 向下取整
     const mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
       return mid;
@@ -19,6 +21,7 @@ function searchInsert(nums: number[], target: number): number {
       right = mid - 1;
     }
   }
+  // 退出循环时，left === right + 1
   return left;
 };
 // @lc code=end
