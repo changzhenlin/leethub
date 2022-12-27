683b21b5573f1e87008e9b248f531cdedaed3e4b
changzhenlin <chang_zhenlin@126.com>
2022-12-27 15:05:46 +0800

    加一注释

--- 66.加一.ts
+++ 66.加一.ts
@@ -7,7 +7,9 @@
 // @lc code=start
 function plusOne(digits: number[]): number[] {
   const str = digits.join('');
+  // 用 BigInt 处理大数
   const num = BigInt(str) + BigInt(1);
+  // map(Number) 是将字符串数组转换为数字数组
   return num.toString().split('').map(Number);
 };
 // @lc code=end
