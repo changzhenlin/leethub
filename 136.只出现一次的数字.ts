cff5ef098d0ea32f6bd3c3094636ba87213d9c81
changzhenlin <chang_zhenlin@126.com>
2023-01-03 19:09:24 +0800

    只出现一次的数字

---
+++ 136.只出现一次的数字.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=typescript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+function singleNumber(nums: number[]): number {
+  const set: Set<number> = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      set.delete(nums[i]);
+    } else {
+      set.add(nums[i]);
+    }
+  }
+  return set.values().next().value;
+  // 该解法只适用于其余数字都出现两次的情况
+};
+// @lc code=end
+
