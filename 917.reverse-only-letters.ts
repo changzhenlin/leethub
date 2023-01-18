0f647f989df0df00f5b08fc38e77c16d3110a063
changzhenlin <chang_zhenlin@126.com>
2023-01-18 16:54:02 +0800

    Reverse Only Letters

--- 917.reverse-only-letters.ts
+++ 917.reverse-only-letters.ts
@@ -7,15 +7,18 @@
 // @lc code=start
 function reverseOnlyLetters(s: string): string {
   const arr = s.split('');
+  // 用filter过滤掉非字母
   const reverseArr = arr.reverse().filter((char) => {
     return /[a-zA-Z]/.test(char);
   });
   const copyArr = arr.reverse();
+  // 判断是否是字母
   const isLetter = (char: string) => {
     return /[a-zA-Z]/.test(char);
   };
   copyArr.forEach((char, index) => {
     if (!isLetter(char)) {
+      // 如果不是字母，就插入到reverseArr中
       reverseArr.splice(index, 0, char);
     }
   }
