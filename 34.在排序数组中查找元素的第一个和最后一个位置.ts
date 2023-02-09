b8864eb3ce5a0d7916d21a2f0e4f707062ad0074
changzhenlin <chang_zhenlin@126.com>
2023-02-09 18:32:39 +0800

    remark

--- 34.在排序数组中查找元素的第一个和最后一个位置.ts
+++ 34.在排序数组中查找元素的第一个和最后一个位置.ts
@@ -6,9 +6,11 @@
 
 // @lc code=start
 function searchRange(nums: number[], target: number): number[] {
+  // 1. 二分查找，找到target的位置
   let left = 0;
   let right = nums.length - 1;
   let mid = 0;
+  // 2. 从target的位置向左右两边扩散，找到target的起始位置和结束位置
   while (left <= right) {
     mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
@@ -19,9 +21,11 @@ function searchRange(nums: number[], target: number): number[] {
       right = mid - 1;
     }
   }
+  // 3. 如果没有找到target，返回[-1, -1]
   if (nums[mid] !== target) {
     return [-1, -1];
   }
+  //  4. 时间复杂度O(logn), 空间复杂度O(1)
   let start = mid;
   let end = mid;
   while (start >= 0 && nums[start] === target) {
@@ -30,6 +34,7 @@ function searchRange(nums: number[], target: number): number[] {
   while (end < nums.length && nums[end] === target) {
     end++;
   }
+  // 5. 返回结果
   return [start + 1, end - 1];
 
 };
